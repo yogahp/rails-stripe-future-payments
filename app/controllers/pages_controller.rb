a68499fe4f3c48d27edc091c419dd66d8abaada3
Yoga Hapriana <thenelse.v2@gmail.com>
2023-12-11 23:47:19 +0700

    Modify app/controllers/pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     end
 
     def checkout
-        Stripe.api_key = 'sk_test_51OMCEWFfaI1YjCZRtOI49rFVitnTumaC5ZdTue3UgVLfFAJBOULCqIobnLI1lGKj9cPlaycnSI3XiW7B9aOUhwtP00XGX5xiEI'
+        Stripe.api_key = ENV['STRIPE_API_KEY']
 
         session = Stripe::Checkout::Session.create(
             payment_method_types: ['card'],
@@ -12,6 +12,7 @@ class PagesController < ApplicationController
             success_url: 'https://example.com/success?session_id=1',
             cancel_url: 'https://example.com/cancel',
         )
+        
         redirect_to root_url
     end
 end
